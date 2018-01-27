6dd508a63f1b728bc4423ee215528ca9804fa297
zhouhoubo <278894203@qq.com>
2018-01-27 23:39:16 +0800

    Making sftp Look More Like ftp

---
+++ ftp.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# mysftp - Makes sftp start up more like ftp.
+
+echo -n "User account: "
+read account
+
+if [ -z $account ] ; then
+  exit 0;       # changed their mind, presumably
+fi
+
+if [ -z "$1" ] ; then
+  echo -n "Remote host: "
+  read host
+  if [ -z $host ] ; then
+    exit 0
+  fi
+else
+  host=$1
+fi
+
+# End by switching to sftp. The -C flag enables compression here.
+
+exec /usr/bin/sftp -C $account@$host
+
